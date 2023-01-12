ae57c90c8bf5f2c0e66003c3b367c9d80e4a5c36
raquelroca <rrocamartinez8@gmail.com>
2023-01-12 13:50:28 +0100

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+
+
+function footer(){
+    
+    return(
+        <footer>
+          <div class="social">
+             <ul>
+                <li><spam class='fab fa-facebook-f'></spam></li>
+             
+                <li><spam class='fab fa-twitter'></spam></li>
+             
+                <li><spam class='fab fa-instagram'></spam></li>
+             
+                <li><spam class='fab fa-youtube'></spam></li>
+             </ul>
+          </div>
+          <div class="derechos">
+            <small>&copy; 2022 <b>Cococtail Libre</b> - Todos los derechos reservados </small>
+          </div>
+        </footer>
+    
+    )
+    
+    
+    }
+    
+    export default footer;
