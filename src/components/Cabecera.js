f5f28740068c35b2b4b23aa010b11365039db83a
raquelroca <rrocamartinez8@gmail.com>
2023-01-27 13:22:38 +0100

    terminada cabecara slider de los putos cojones de mierda

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,4 +1,7 @@
 import barra from '../img/barra.jpg';
+import barra2 from '../img/barra2.jpg';
+import barra4 from '../img/barra3.jpg';
+import barra3 from '../img/barra4.jpg';
 
 
 function cabecera(){
@@ -6,7 +9,12 @@ function cabecera(){
 <>
 
     <div class="cabecera">
-        <img src={barra}/>
+        <div class="imagenes">
+           <div class="imagen"><img src={barra}/></div> 
+           <div class="imagen"><img src={barra2}/></div> 
+           <div class="imagen"><img src={barra3}/></div> 
+           <div class="imagen"><img src={barra4}/></div> 
+        </div >
         <div class="COCOKTAIL">COCOKTAIL.</div>
     </div>
 
