a3440cb363051b954f0aa6768664f95e9d615fc0
raquelroca <rrocamartinez8@gmail.com>
2023-02-02 13:54:36 +0100

    fotos categorias

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,8 @@ import Nav from './Nav';
 import Cab from './Cabecera';
 import Categorias from './Categorias';
 import Barra from './Barra';
-import Popup from './Popup';
-import Topten from './Topten';
 import Footer from './Footer';
 
-import Cartas from './Cartas';
 
 import '../sass/main.scss';
 
