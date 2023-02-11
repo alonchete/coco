b32b40b3246edea0ce07f982b9f3d20c33afabe8
Ian Carlo Almojera <ianalmojera928@gmail.com>
2023-02-11 22:55:24 +0100

    popup nuevo v2

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -8,18 +8,17 @@
             
             <div class="contenido">
             <a href="#cierro"  onClick={() => hijoApadre(false)} >&times;</a> 
-                <div class="izquierda">
+                
                     <div class="imagen">
-                    <img  src={props.img} />
+                        <img  src={props.img} />
                     </div>
-                </div>
-                <div class="derecha">
+
                     <div class="ingredientes">
                         <p className="nombre_coco">{props.name}</p>
                         <p>{props.prep}</p>
                         <button class="verReceta">VER RECETA</button>
                     </div>
-                </div>
+               
             </div>
         </div>
     ) : "";
